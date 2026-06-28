cc0ab5f8d6a46d0bdf2d5f68f4ed7c9a96b3ff66
sarasara <sarasarasinsin@gmail.com>
2026-06-28 17:06:18 +0900

    tensor

---
+++ tensor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class MatrixOperations
+{
+    public static void Main()
+    {
+        //テスト用3x2行列(転置テスト用)
+        double[,] matrixA =
+        {
+            { 1, 2, },
+            { 3, 4, },
+            {5, 6,  }
+        };
+
+        Console.WriteLine("■元の行列A(3x2):");
+        PrintMatrix(matrixA);
+    }
+
+    
+
+
+
+
+
+
+
+
+
+    private static void PrintMatrix(double[,] matrixA)
+    {
+        throw new NotImplementedException();
+    }
+}
